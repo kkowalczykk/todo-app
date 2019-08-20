6a3f3d3c4bfc247c7725752f6803984e569a7f0b
kkowalczykk <kovalczyk97@gmail.com>
2019-08-20 15:05:40 +0200

    Updates

---
+++ precache-manifest.000cf0a4f427d193d25675ead25555ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f93ba0a1a214eb0bc7e0fecadf281f",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8e77f2bd780348c5fa97",
+    "url": "/todo-app/static/css/main.bb55292a.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/todo-app/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "8e77f2bd780348c5fa97",
+    "url": "/todo-app/static/js/main.33576baf.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
